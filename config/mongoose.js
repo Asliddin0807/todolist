b158f0da7c7535dd7e563f514ac87b8d2dfdafe8
Asliddin0807 <114002286+Asliddin0807@users.noreply.github.com>
2023-03-18 15:01:59 +0500

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const isConnect = () => {
     mongoose.set("strictQuery", false)
-    const isConnect = mongoose.connect('mongodb://127.0.0.1:27017/todoList').then(() => {
+    const isConnect = mongoose.connect('mongodb+srv://asliddin:asliddin123@atlascluster.k5lmz3l.mongodb.net/?retryWrites=true&w=majority').then(() => {
         console.log('database connected')
     }).catch((err) => {
         throw new Error(err)
